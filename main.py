3fd5e3b76bb38e2bc0c07fcbfa6bd95ed2b2059b
cgkh992 <103305429+cgkh992@users.noreply.github.com>
2022-11-18 12:42:22 +0800

    Revert "edited"
    This reverts commit 87c195a481e264ec3a951aceb13f0da856ba8945.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(names):
+def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {names}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
